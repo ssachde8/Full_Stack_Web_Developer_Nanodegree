500adfd1199725e1b8d59804d8e1f6de780b1b35
Satvik Sachdev <ssachde8@asu.edu>
2017-08-09 12:08:24 -0700

    Add files via upload

--- P1_Movie_Trailer_Website/media.py
+++ P1_Movie_Trailer_Website/media.py
@@ -1,15 +1,16 @@
 import webbrowser
 
-class Movie():
-	"""
-	Movie Trailers Website
-	params:
-	title(str): Title of movie_title
-	poster_image_url (str): Image from IMDB
-	trailer_youtube_url(str): Movoe trailer from youtube
-	"""
-	
-	def __init__(self, title, poster_image_url, trailer_youtube_url):
-		self.title = title
-		self.poster_image_url  = poster_image_url 
-		self.trailer_youtube_url = trailer_youtube_url
+
+class Movie:
+    """
+    Movie Trailers Website
+    params:
+    title(str): Title of movie_title
+    poster_image_url (str): Image from IMDB
+    trailer_youtube_url(str): Movie trailer from youtube
+    """
+
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        self.title = title  # set movie title
+        self.poster_image_url = poster_image_url  # set poster image url
+        self.trailer_youtube_url = trailer_youtube_url  # set trailer url
